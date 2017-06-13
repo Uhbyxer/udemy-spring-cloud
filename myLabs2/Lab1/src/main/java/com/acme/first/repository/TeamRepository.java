41de30ccc0e8ec27467c830e982a9f5e938377f3
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2017-06-13 14:05:26 +0300

    add data-rest

--- myLabs2/Lab1/src/main/java/com/acme/first/repository/TeamRepository.java
+++ myLabs2/Lab1/src/main/java/com/acme/first/repository/TeamRepository.java
@@ -2,6 +2,8 @@ package com.acme.first.repository;
 
 import com.acme.first.domain.Team;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource(path = "teams", rel = "team")
 public interface TeamRepository extends CrudRepository<Team, Long> {
 }
