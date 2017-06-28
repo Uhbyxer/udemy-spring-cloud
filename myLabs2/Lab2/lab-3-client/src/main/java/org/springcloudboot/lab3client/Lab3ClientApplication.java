aea81b06696e3b1652ac6c91c14a08493e39c58f
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2017-06-28 13:00:51 +0300

    Add Lab3 default profile client

---
+++ myLabs2/Lab2/lab-3-client/src/main/java/org/springcloudboot/lab3client/Lab3ClientApplication.java
@@ -0,0 +1,14 @@
+package org.springcloudboot.lab3client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class Lab3ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ClientApplication.class, args);
+	}
+}
