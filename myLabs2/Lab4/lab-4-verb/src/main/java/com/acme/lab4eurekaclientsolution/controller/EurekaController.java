1ec831417cc790f51a76d72f18c37b0fc75a5591
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2017-07-02 23:10:42 +0300

    Add sentence client

---
+++ myLabs2/Lab4/lab-4-verb/src/main/java/com/acme/lab4eurekaclientsolution/controller/EurekaController.java
@@ -0,0 +1,17 @@
+package com.acme.lab4eurekaclientsolution.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class EurekaController {
+	@Value("${words}")
+	private String words;
+
+	@RequestMapping
+	public String getWord() {
+		String[] wordsArray = words.split(",");
+		return wordsArray[(int) Math.round(Math.random() * (wordsArray.length - 1)) ];
+	}
+}
