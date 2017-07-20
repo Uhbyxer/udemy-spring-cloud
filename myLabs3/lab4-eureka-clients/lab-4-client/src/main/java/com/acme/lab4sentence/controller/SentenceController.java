408a84d22496b9e1bd917e5202674839077ac4ef
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2017-07-20 23:19:11 +0300

    lab 4: final ver

--- myLabs3/lab4-eureka-clients/lab-4-client/src/main/java/com/acme/lab4sentence/controller/SentenceController.java
+++ myLabs3/lab4-eureka-clients/lab-4-client/src/main/java/com/acme/lab4sentence/controller/SentenceController.java
@@ -1,6 +1,7 @@
 package com.acme.lab4sentence.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,19 +11,21 @@ import org.springframework.web.client.RestTemplate;
 import java.net.URI;
 import java.util.List;
 
-@RestController("/sentence")
+@RestController
 public class SentenceController {
-	@Autowired
-	private DiscoveryClient client;
+	@Autowired private DiscoveryClient client;
 
-	@GetMapping
-	public String getSentence() {
-		return getWord("LAB-4-SUBJECT") + " "
-				+ getWord("LAB-4-VERB") + " "
-				+ getWord("LAB-4-ARTICLE") + " "
-				+ getWord("LAB-4-ADJECTIVE") + " "
-				+ getWord("LAB-4-NOUN") + "."
-		;
+	@Value("${words}") private String words;
+
+	@GetMapping("/") public String getWord() {
+		String[] wordArray = words.split(",");
+		int i = (int) Math.round(Math.random() * (wordArray.length - 1));
+		return wordArray[i];
+	}
+
+	@GetMapping("/sentence") public String getSentence() {
+		return getWord("LAB-4-SUBJECT") + " " + getWord("LAB-4-VERB") + " " + getWord("LAB-4-ARTICLE") + " " + getWord(
+				"LAB-4-ADJECTIVE") + " " + getWord("LAB-4-NOUN") + ".";
 	}
 
 	private String getWord(String service) {
